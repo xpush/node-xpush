f7cfa11389b2ef31768ab4cc08a53d7bbb02f332
John Kim <yohany@gmail.com>
2014-01-17 00:31:39 +0900

    :raising_hand:

--- test/web.js
+++ test/web.js
@@ -1,6 +1,8 @@
 var assert  = require("assert"),
     io      = require('socket.io-client'),
-    restify = require('restify');
+    restify = require('restify'),
+    async   = require('async');
+
 
 var gatewayServer = restify.createJsonClient( {
   url: 'http://gateway.server:8000',
@@ -12,93 +14,210 @@ var socketOptions ={
   'force new connection': true
 };
 
-describe('Web Client', function() {
-  this.timeout(3000);
 
-  var appId = '';
+var Application = {
+  appNm: 'xpush-messenger' 
+};
 
-  var apiServers = {};
-  var clients = {};
 
-  before( function(done) {
+var Users = {
+  John: {
+    userId : 'John'
+  },
+  Ally: {
+    userId : 'Ally'
+  },
+  Lynn: {
+    userId : 'Lynn'
+  },
+  Danial: {
+    userId : 'Danial'
+  }
 
-    gatewayServer.post(
-      '/app/create/xpush-messenger-sample',
+};
+
+
+var API = {
+
+  app_create: function (_appNm, callback) {
+    gatewayServer.post( '/app/create/'+_appNm,
+      function(err, req, res, data) {
+        if( err ){
+          console.log(err);
+        }else{
+          callback(data);
+        }
+      });
+  },
+  node: function (_app, _channel, callback) {
+    gatewayServer.get('/node/'+_app+'/'+_channel, 
       function(err, req, res, data) {
         if( err ){
           console.log( err );
         } else {
-          appId = data.appId;
+          callback(data);
           done();
         }
-      }
-    );
-/*
-    gatewayServer.get(
-      '/app/list',
+      });
+  },
+  node_session: function (_userId, callback) {
+    gatewayServer.get('/node/session/'+_userId, 
       function(err, req, res, data) {
         if( err ){
           console.log( err );
         } else {
-          for (var app in data.result) {
-            appId = app.appId;
-            console.log( app.appNm +' is selected. ');
-            done();
-          }
+          callback(data, _userId);
         }
-      }
-    );
-*/
-  });
+      });
+  },
+  user_register: function (_userId, callback) {
 
-  describe('#login() - JohnKim', function() {
+    var params = {
+      app: Application.appId,
+      userId: _userId,
+      deviceType: 'WEB'
+    };
 
-    it('get Api Server URL', function(done) {
+    gatewayServer.post('/user/register', params, 
+      function(err, req, res, data) {
+        if( err ){
+          console.log( err );
+        } else {
 
-      gatewayServer.get('/node/session/JohnKim', 
-        function(err, req, res, data) {
-          if( err ){
-            console.log( err );
-          } else {
-
-            console.log(data);
-            apiServer.url1 = data.result; 
-
-            clients.socket1 = io.connect(apiServer.server, socketOptions);
-
-          }
-          done();
-        });
-    });
-
-    it('login with JohnKim', function(done) {
-
-      clients.socket1.on('connect', function(data){
-
-        var param = {
-          app: appId,
-          userId: 'JohnKim',
-          server: apiServer.name };
-
-        clients.socket1.emit('login', param, function (data) {
+        console.log(req);
           console.log(data);
-          done();
+          callback(data, _userId);
+        }
+      });
+  }
+  
+};
+
+var Library = {
+
+  login: function(_userId, callback) {
+
+    API.node_session(Users[_userId].userId, function (data, _userId) {
+
+      Users[_userId].sessionSocket = io.connect(data.result.server, socketOptions);
+      Users[_userId].sessionSocket.on('connect', function() {
+        
+        var param = {
+          app: Application.appId,
+          userId: _userId,
+          server: data.result.name };
+
+        Users[_userId].sessionSocket.emit('login', param, function (data) {
+          console.info('\t logined : '+JSON.stringify(data));
+          callback(data);
         });
 
       });
-      
+
+      Users[_userId].sessionSocket.on('notification', function (data) {
+        console.info('\t Notification : '+JSON.stringify(data));
+      });
+
+    });
+  },
+
+  channels: function(_userId, callback) {
+
+    var param = {
+      app: Application.appId,
+      userId: _userId};
+
+    Users[_userId].sessionSocket.emit('channels', param, function (data) {
+        console.info('\t channels : '+JSON.stringify(data));
+      callback(data);
+    });
+    
+  }
+};
+
+// ## Sample Application TEST !!! 
+
+describe('xpush samples', function() {
+  this.timeout(2000);
+
+  before( function(done) {
+    API.app_create(Application.appNm, function(data) {
+      Application.appId = data.result.appId;
+      done();
+    });
+  });
+
+  describe('#registration()', function() {
+
+    it('John', function(done) {
+      API.user_register('John', function(data) {
+        done();
+      });
     });
 
-    it('get the list of JohnKim\'s channels', function (done) {
-      
+    it('Ally', function(done) {
+      API.user_register('Ally', function(data) {
+        done();
+      });
+    });
 
+    it('Lynn', function(done) {
+      API.user_register('Lynn', function(data) {
+        done();
+      });
+    });
+
+    it('Daniel', function(done) {
+      API.user_register('Daniel', function(data) {
+        done();
+      });
     });
 
   });
 
-  describe('#createChannel()', function() {
-    it('should create the new channel', function() {
+  describe('#login()', function() {
+
+    it('John', function(done) {
+      Library.login('John', function(result){
+        done();
+      });
     });
+
+    it('Ally', function(done) {
+      Library.login('Ally', function(result){
+        done();
+      });
+    });
+
+    it('Lynn', function(done) {
+      Library.login('Lynn', function(result){
+        done();
+      });
+    });
+
+    it('Danial', function(done) {
+      Library.login('Danial', function(result){
+        done();
+      });
+    });
+
+  });
+
+
+  describe('#channels()', function() {
+
+    it('John', function(done) {
+      Library.channels('John', function(result){
+        done();
+      });
+    });
+
+    it('Ally', function(done) {
+      Library.channels('Ally', function(result){
+        done();
+      });
+    });
+
   });
 
 });
