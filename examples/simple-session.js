95749aaf57754caa88767fc5b0ae49943d68114d
John Kim <yohany@gmail.com>
2014-02-10 00:19:53 +0900

    :flushed: add facebook login functions

--- examples/simple-session.js
+++ examples/simple-session.js
@@ -13,20 +13,31 @@ try {
   var data = fs.readFileSync(argv.config);
   config = JSON.parse(data.toString());
 } catch (ex) {
-  util.puts('Error starting xpush server: ' + ex);
+  console.log('Error starting xpush server: ' + ex);
   process.exit(1);
 }
 
+config.host = 'session.xpush.io';
 config.port = 8000;
 
 var server = xpush.createSessionServer(config);
 
-server.on('connected', function (url, port){
+
+server.static(/\/public\/?.*/, {
+  directory: __dirname+'/public'
+});
+
+server.once('connected', function (url, port){
   console.log('listen - '+url+':'+port);
 });
+
+server.on('oauth', function (data){
+  data.response.send('Welcome ' + data.request.user.displayName);
+});
+
+
 
 server.get('/test', function (req, res, next) {
   res.send({status : 'pong'});
   next();
 });
-
