dd1cb52542f5fe92864448a9de46e42097bfcc80
John Kim <yohany@gmail.com>
2014-12-31 22:16:53 +0900

    fix error messages on starting server

--- test/simple/stressTest.js
+++ test/simple/stressTest.js
@@ -5,12 +5,15 @@ var io    = require( '../../node_modules/socket.io/node_modules/socket.io-client
 
 /************************
   로컬의 8000 포트의 Session 서버를 통해서 100개의 Channel 을 연결하고 메시지를 보내기
-  USAGE : node stressTest.js 8000 100
+  USAGE : node stressTest.js 127.0.0.1:8000 100
 *************************/
 
-var port        = process.argv[2];        // session 서버 포트번호
+var address     = process.argv[2];        // session 서버 포트번호
 var count       = process.argv[3] || 1;   // Client 쓰레드 수 (Optional)
 var maxChannel  = process.argv[4] || 10;  // Client 가 연결하는 Channel 수 (Optional)
+
+var _host = address.substr(0, address.indexOf(':'));
+var _port = Number(address.substr(address.indexOf(':') + 1));
 
 var run = function(){
 
@@ -19,9 +22,9 @@ var run = function(){
   var channel = "CHANNEL_"+faker.random.number(parseInt(maxChannel));
   var userId  = faker.internet.userName();
 
-  console.log('CHANNEL : ', '127.0.0.1:'+port+'/node/' + app + '/' + channel);
+  console.log('CHANNEL : ', address+'/node/' + app + '/' + channel);
 
-  util.get( '127.0.0.1', port, '/node/' + app + '/' + channel, function( err, data ){
+  util.get( _host, _port, '/node/' + app + '/' + channel, function( err, data ){
 
     var query =
       'A='+app+'&'+
@@ -29,7 +32,7 @@ var run = function(){
       'U='+userId+'&'+
       'D=DEVAPP&'+
       'S='+data.result.server.name+'&'+
-      'DT={AA:"ABCDE"}&'+
+      'DT={user:"'+userId+'"}&'+
       'MD=CHANNEL_ONLY';
       ;
 
@@ -41,13 +44,9 @@ var run = function(){
     console.log('CHANNEL : ',data.result.server.url+'/channel?'+query);
 
     var channelSocket = io.connect(data.result.server.url+'/channel?'+query, socketOptions);
+
     channelSocket.on( 'connect', function (){
       console.log('connected');
-
-      channelSocket.emit( 'users', function( data ){
-        console.log('************ USERS ************')
-        console.log(data);
-      });
 
       setInterval(function() {
         channelSocket.emit('send', {'NM':'message', 'DT': { 'MG' : faker.lorem.sentence() } });
@@ -56,11 +55,11 @@ var run = function(){
     });
 
     channelSocket.on( 'message', function( data ){
-      console.log(data);
+      //console.log(data);
     });
 
-    channelSocket.on( 'error', function (){
-      console.log('err');
+    channelSocket.on( 'error', function ( data ){
+      console.error(data);
     });
 
   });
@@ -69,5 +68,5 @@ var run = function(){
 
 for(var a=0; a<count; a++){
   run();
-  sleep.sleep(1);
+  //sleep.sleep(1);
 }
