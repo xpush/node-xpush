e8b8a36959d64217635e78c147602bcf3380146f
hongyeon <0nlyoung7@gmail.com>
2015-02-02 16:43:51 +0900

    Add authorization filter to send event

--- example/channel-server.js
+++ example/channel-server.js
@@ -1,32 +1,60 @@
-var xpush    = require('../lib/xpush');
+var xpush    = require('../lib/xpush'),
+  fs       = require('fs'),
+  argv     = require('optimist').argv;
 
-var config = {
-  "zookeeper": {},
-  "redis": {},
-  "mongodb": {}
-};
+var config = {},
+    port = argv.port || 80,
+    server;
 
-config.port = 9000;
-//config.weight = 160;
-//config.name   = '';
+try {
+  var data = fs.readFileSync(argv.config);
+  config = JSON.parse(data.toString());
+} catch (ex) {
+  util.puts('Error starting xpush server: ' + ex);
+  process.exit(1);
+}
 
-var server = xpush.createChannelServer(config);
+config.data = argv.data;
+config.silent = typeof argv.silent !== 'undefined' ? argv.silent : config.silent;
+config.port = port || config.port;
+config.host = argv.host || config.host;
+
+if(argv.weight) config.weight = argv.weight;
+if(argv.name)   config.name   = argv.name;
+
+server = xpush.createChannelServer(config);
 
 server.on('started', function (url, port){
 
   console.log(' >>>>>> CHANNEL SERVER is started '+url+':'+port);
   console.log(server.ping());
-
   console.log(server.event);
 });
 
 server.channel_on('send', function(params, callback){
-  console.log('>>>>>>>> THIS IS CUSTOMIZED EVENT !!!!--- ');
 
+  console.log('>>>>>>>> THIS IS CUSTOMIZED EVENT !!!!--- ');
+  var query = this.handshake.query;
+
+  var DT = query.DT;
+  if( DT && typeof DT == 'string' ){
+    DT = JSON.parse( DT );
+  }
+
+  if( DT.roomLevel == '9'|| DT.roomLevel == 9 ){
+    if (callback) callback({ status: 'ERR-AUTH', message: 'Please authorize the user first' });
+    return;
+  }
+
+  server.send( this,  params, callback );
 });
 
 server.channel_on('send2', function(params, callback){
-  console.log('>>>>>>>> THIS IS CUSTOMIZED EVENT !!!! ');
-  server.send(this, params, callback);
 
+  console.log('>>>>>>>> THIS IS CUSTOMIZED EVENT !!!! ');
+
+});
+
+server.channel_on('connection', function( socket ){
+  console.log('>>>>>>>> THIS IS CUSTOMIZED CONNECTION !!!! ');
 });
