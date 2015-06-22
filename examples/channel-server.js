09fb453c2c3ffcd95f2ebd0f17a62317e8fd67b0
John Kim <yohany@gmail.com>
2015-06-22 12:34:02 +0900

    get started for version 0.1

---
+++ examples/channel-server.js
@@ -0,0 +1,31 @@
+var xpush    = require('../lib/xpush');
+
+var config = {
+  "zookeeper": {},
+  "redis": {},
+  "mongodb": {}
+};
+
+config.port = 9001;
+
+var server = xpush.createChannelServer(config);
+
+
+function foo(req, res, next) {
+    //res.send(204);
+    res.send({hello: 'XPUSH WORLD !!! '+config.port});
+    next();
+}
+
+server.put('/foo', foo);
+server.get('/foo', foo);
+server.del('/foo', foo);
+server.post('/foo', foo);
+
+
+
+server.on('started', function (url, port){
+
+  console.log(' >>>>>> Channel SERVER is started '+url+':'+port);
+
+});
--- examples/session-server.js
+++ examples/session-server.js
@@ -10,6 +10,22 @@ config.port = 8888;
 
 var server = xpush.createSessionServer(config);
 
+
+
+
+function foo(req, res, next) {
+    //res.send(204);
+    res.send({hello: 'XPUSH WORLD !!! '+config.port});
+    next();
+}
+
+server.put('/foo', foo);
+server.get('/foo', foo);
+server.del('/foo', foo);
+server.post('/foo', foo);
+
+
+
 server.on('started', function (url, port){
 
   console.log(' >>>>>> SESSION SERVER is started '+url+':'+port);
